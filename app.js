9e594803b7603dade67a8601c467191ab525eac6
Cody Karst <ckarst@maf.org>
2016-12-29 15:14:14 -0700

    fixed typoe

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 < script >
     // removes BBNC stylesheets from the page
-    $("link[href*='styleshhet']").prop('disabled', true);
+    $("link[href*='stylesheet']").prop('disabled', true);
     $("link[href*='stylesheet']").remove();
 < /script>
